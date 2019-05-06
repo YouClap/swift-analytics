035c489aaf9e46a4ff25d3722ea230793ca4f988
Luís Portela Afonso <luis.portela@youclap.tech>
2019-05-06 11:23:35 +0100

    Cleanup commented code 😅 (#3)

--- Sources/Trackers/AnalyticsTracker.swift
+++ Sources/Trackers/AnalyticsTracker.swift
@@ -1,18 +1,6 @@
 // Copied and modified from https://github.com/Mindera/Alicerce/blob/master/Sources/Analytics/Trackers/AnalyticsTracker.swift 🙏
 
 public protocol AnalyticsTracker: AnyObject {
-//    /// A type that represents all the possible types that the tracker's `Event.state` event can have.
-//    associatedtype State
-//
-//    /// A type that represents all the possible types that the tracker's `Event.action` event can have.
-//    associatedtype Action
-//
-//    /// A type that represents all the possible parameter keys that a tracker's `Event` parameter dictionary can have.
-//    associatedtype ParameterKey: AnalyticsParameterKey
-//
-//    /// An analytics event.
-//    typealias Event = Analytics.Event<State, Action, ParameterKey>
-
     /// A type representing a tracker's identifier.
     typealias ID = String
 
